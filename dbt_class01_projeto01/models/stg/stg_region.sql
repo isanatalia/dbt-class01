32b548f0f91fe510169f76701130fec16c5743e2
Isa <nataliamagalhaesd@gmail.com>
2025-01-25 16:35:37 -0300

    adicionado stg_region

--- dbt_class01_projeto01/models/stg/stg_region.sql
+++ dbt_class01_projeto01/models/stg/stg_region.sql
@@ -2,4 +2,4 @@
         R_REGIONKEY    as region_key,
         R_NAME         as region_name,
         R_COMMENT      as region_comment,
-    FROM {{ ref('src_region') }}
+    FROM {{ref('src_region') }}
